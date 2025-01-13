b401500c73458f5595390f7c7f298b1c444c88bb
tranhoan1603 <doannguyentranhoan@gmail.com>
2025-01-13 21:15:39 +0700

    update vector store

--- src/rag/vector_store.py
+++ src/rag/vector_store.py
@@ -3,16 +3,15 @@ import faiss
 from langchain_community.docstore.in_memory import InMemoryDocstore
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
 
 class VectorDB:
-    def __init__(self, documents=None, vector_db=FAISS, embedding=HuggingFaceEmbeddings()) -> None:
+    def __init__(self, documents=None, 
+                 vector_db: Union[Chroma, FAISS] = Chroma, 
+                 embedding=HuggingFaceEmbeddings()
+                 ) -> None:
         self.embedding = embedding
-        self.vector_db = vector_db(
-            embedding_function=embedding,
-            index=faiss.IndexFlatL2(768),
-            docstore=InMemoryDocstore,
-            index_to_docstore_id={}
-        )
+        self.vector_db = vector_db()
         self.db = self.build_db(documents)
 
     def build_db(self, documents):
@@ -22,7 +21,10 @@ class VectorDB:
         )
         return db
     
-    def get_retriever(self, search_type: str='mmr', search_kwargs: dict={'k': 10}):
+    def get_retriever(self, 
+                      search_type: str='similarity', 
+                      search_kwargs: dict={'k': 10}
+                      ):
         retriever = self.db.as_retriever(
             search_type=search_type, 
             search_kwargs=search_kwargs
